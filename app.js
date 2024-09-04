e652fe140affbd4880b9cc2166a52846dbb29898
James Mccarthy <mccarthyjames35@yahoo.com>
2024-09-04 11:48:55 -0700

    finished refactor

--- app.js
+++ app.js
@@ -74,27 +74,11 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector('.section-center');
-const filterBtns = document.querySelectorAll('.filter-btn');
+const container = document.querySelector('.btn-container');
 
 window.addEventListener('DOMContentLoaded', function () {
 	displayMenuItems(menu);
-});
-
-filterBtns.forEach(function (btn) {
-	btn.addEventListener('click', function (e) {
-		const category = e.currentTarget.dataset.id;
-
-		const menuCategory = menu.filter(function (menuItem) {
-			if (menuItem.category === category) {
-				return menuItem;
-			}
-		});
-		if (category === 'all') {
-			displayMenuItems(menu);
-		} else {
-			displayMenuItems(menuCategory);
-		}
-	});
+	displayMenuButtons();
 });
 
 function displayMenuItems(menuItems) {
@@ -114,4 +98,40 @@ function displayMenuItems(menuItems) {
 	});
 	displayMenu = displayMenu.join('');
 	sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons() {
+	const categories = menu.reduce(
+		function (values, item) {
+			if (!values.includes(item.category)) {
+				values.push(item.category);
+			}
+			return values;
+		},
+		['all'],
+	);
+	const categoryBtns = categories
+		.map(function (category) {
+			return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`;
+		})
+		.join('');
+	container.innerHTML = categoryBtns;
+	const filterBtns = document.querySelectorAll('.filter-btn');
+
+	filterBtns.forEach(function (btn) {
+		btn.addEventListener('click', function (e) {
+			const category = e.currentTarget.dataset.id;
+
+			const menuCategory = menu.filter(function (menuItem) {
+				if (menuItem.category === category) {
+					return menuItem;
+				}
+			});
+			if (category === 'all') {
+				displayMenuItems(menu);
+			} else {
+				displayMenuItems(menuCategory);
+			}
+		});
+	});
 }
